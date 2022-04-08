2aa417e5a8804b0e39357abb6ca5b12fd5fe7f57
suneelkumarreddy-k <67097067+suneelkumarreddy-k@users.noreply.github.com>
2022-04-08 08:50:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function cal() {
 var start = performance.now();
-document.getElementById("cals").innerHTML = (document.getElementById("carbs").value*4)+(document.getElementById("prts").value*4)+ (document.getElementById("fats").value *9);
-document.getElementById("cbs").innerHTML = document.getElementById("carbs").value*4 + " Calories";
-document.getElementById("pts").innerHTML = document.getElementById("prts").value*4 + " Calories";
-document.getElementById("fts").innerHTML = document.getElementById("fats").value*9 + " Calories";
+document.getElementById("cals").innerText = (document.getElementById("carbs").value*4)+(document.getElementById("prts").value*4)+ (document.getElementById("fats").value *9);
+document.getElementById("cbs").innerText = document.getElementById("carbs").value*4 + " Calories";
+document.getElementById("pts").innerText = document.getElementById("prts").value*4 + " Calories";
+document.getElementById("fts").innerText = document.getElementById("fats").value*9 + " Calories";
 var end = performance.now();
 var wpn = end - start; 
-alert("Calculated in "+wpn+" ms 😎");
+alert("Calculated in "+wpn.toFixed(2)+" ms 😎");
 }
